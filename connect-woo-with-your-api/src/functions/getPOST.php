5a2c61f56a1af8dd41919725dc4aed0656c62c76
franciscoblancojn <blancofrancisco34@gmail.com>
2024-10-02 18:07:06 -0500

    fix post

--- connect-woo-with-your-api/src/functions/getPOST.php
+++ connect-woo-with-your-api/src/functions/getPOST.php
@@ -4,7 +4,6 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 function CWWYA_getPOST($key){
     try{
-        // $_POST It is necesary for get all data in api body
         return CWWYA_sanitizeObj($_POST[$key]);
     } catch (\Throwable $th) {
         return [];
